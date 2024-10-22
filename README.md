658182d18c1f36bacfcc7bb58889ce919028a77c
Iury-Gabriel <157987319+Iury-Gabriel@users.noreply.github.com>
2024-10-21 22:36:30 -0300

    updating readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# User-Service
+
+# User Service Project
+
+## How to run the project
+
+after cloningg the project:
+
+npm i
+
+## Update the docker-compose file to your credentials
+
+docker-compose up
+
+npm run dev
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ import GetUsersUseCase from './domain/registro/application/use-cases/GetUsersUse
 import DeleteUserUseCase from './domain/registro/application/use-cases/DeleteUserUseCase';
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
 
